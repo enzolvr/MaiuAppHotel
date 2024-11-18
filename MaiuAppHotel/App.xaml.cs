f45339be76caf3de2f764a68f41b9bdec5d642db
enzolvr <EnzoS.Oliveira@hotmail.com>
2024-11-18 08:17:36 -0300

    Adicionar arquivos de projeto.

---
+++ MaiuAppHotel/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MaiuAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 700;
+
+            return window;
+        }
+    }
+}
---
+++ MaiuAppHotel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MaiuAppHotel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
